60b35343da165474c834262dfc0bb3896ed04d69
Coffeemiller <dev2freedom@gmail.com>
2022-05-20 23:47:38 +0900

    Canvas line function Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 2022-05-20 Start!
+
+### URL Doc.
++ [영문 캔버스 API문서](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API)
++ [한글 캔버스 API문서](https://developer.mozilla.org/ko/docs/Web/API/Canvas_API)
+
--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strockeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,27 +13,26 @@ function stopPainting() {
     painting = false;
 }
 
+function startPainting() {
+    painting = true;
+}
+
 function onMouseMove(event) {
-    // console.log(event);
     const x = event.offsetX;
     const y = event.offsetY;
-    // console.log(x, y);
-}
-
-function onMouseDown(event) {
-    // console.log(event);
-    stopPainting() //painting = true;
-
-}
-
-function onMouseUp(event) {
-    painting = false;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
